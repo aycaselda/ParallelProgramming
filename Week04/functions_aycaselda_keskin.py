0afe708d01ee91b458b67dfe98f7c33b35b0de87
aycaselda <keskinaycaselda@gmail.com>
2026-03-31 17:55:19 +0300

    Update functions_aycaselda_keskin.py

--- Week04/functions_aycaselda_keskin.py
+++ Week04/functions_aycaselda_keskin.py
@@ -19,7 +19,7 @@ def custom_equation(x: int = 0, y: int = 0, /, a: int = 1, b: int = 1, *, c: int
     """
     return (x**a + y**b) / c
 
-def fn_w_counter() -> tuple[int, dict[str, int]]:
+def fn_w_counter() -> (int, dict[str, int]):
     if not hasattr(fn_w_counter, "call_count"):
         fn_w_counter.call_count = 0
         fn_w_counter.callers_dict = {}
